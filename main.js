a0ae58e7f26b338219a312fc3d2e50e0c01f9e56
Jiloro59 <jolyremi59@gmail.com>
2022-01-24 16:43:39 +0100

    exo fait

--- main.js
+++ main.js
@@ -1,37 +1,58 @@
 const firstName = 'r0ulito';
 const lastName = 'formateur';
 
-function FirstName(props) {
+// function FirstName(props) {
 
-    /*
-    // Solution avec bonus
-    const formatFirstName = (firstName) => {
-        return firstName[0].toUpperCase() + firstName.substr(1);
+//     /*
+//     // Solution avec bonus
+//     const formatFirstName = (firstName) => {
+//         return firstName[0].toUpperCase() + firstName.substr(1);
+//     }
+
+//     return <span>{formatFirstName(props.text)}</span>
+//     */
+
+//     // Solution sans bonus
+//     return <span>{props.text[0].toUpperCase() + props.text.substr(1)}</span>
+// }
+
+// function LastName(props) {
+
+//     /*
+//     // Solution avec bonus
+//     const formatLastName = (lastName) => {
+//         return lastName.toUpperCase();
+//     }
+
+//     return <span>{formatLastName(props.text)}</span>
+//     */
+
+//     // Solution sans bonus
+//     return <span className="red-text">{props.text.toUpperCase()}</span>
+
+// }
+
+// const helloWorld = <h1>Hello <FirstName text={firstName}/> <LastName text={lastName}/></h1>;
+
+// ReactDOM.render(helloWorld, document.querySelector('#app'));
+
+class Clock extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {date: new Date()};
     }
-
-    return <span>{formatFirstName(props.text)}</span>
-    */
-
-    // Solution sans bonus
-    return <span>{props.text[0].toUpperCase() + props.text.substr(1)}</span>
-}
-
-function LastName(props) {
-
-    /*
-    // Solution avec bonus
-    const formatLastName = (lastName) => {
-        return lastName.toUpperCase();
+  
+    render() {
+      return (
+        <div>
+          <h1>Hello, world!</h1>
+          <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+        </div>
+      );
     }
-
-    return <span>{formatLastName(props.text)}</span>
-    */
-
-    // Solution sans bonus
-    return <span className="red-text">{props.text.toUpperCase()}</span>
-
-}
-
-const helloWorld = <h1>Hello <FirstName text={firstName}/> <LastName text={lastName}/></h1>;
-
-ReactDOM.render(helloWorld, document.querySelector('#app'));
+  }
+  
+  ReactDOM.render(
+    <Clock />,
+    document.getElementById('app')
+  );
