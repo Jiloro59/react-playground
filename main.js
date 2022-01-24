3148f5bd85f3f56492702807c124fea039fde007
Jiloro59 <jolyremi59@gmail.com>
2022-01-24 10:51:17 +0100

    Exercice fait

---
+++ main.js
@@ -0,0 +1,8 @@
+const helloWorld = React.createElement('h1', {}, 'Hello World!');
+
+ReactDOM.render(helloWorld, document.querySelector("#app"));
+// let monPremierElement = React.createElement(
+//     'section', 
+// {id : "section"},
+//  React.createElement('h1', {}, "Bonjour"));
+// ReactDOM.render(monPremierElement, document.querySelector('#app'));
