e808efc11d962e957fb5b8d70ef20c12118cf80a
Jiloro59 <jolyremi59@gmail.com>
2022-01-24 14:30:17 +0100

    exo fait

--- main.js
+++ main.js
@@ -1,10 +1,22 @@
 //const helloWorld = React.createElement('h1', {}, 'Hello world!');
 
-const firstName = 'r0ulito';
-const lastName = 'formateur';
+// const firstName = 'rémi';
+// const lastName = 'joly';
 
 
-// Solution sans bonus
-const helloWorld = <h1>Hello <span>{firstName[0].toUpperCase() + firstName.substr(1)}</span> <span className="red-text">{lastName.toUpperCase()}</span></h1>;
+// // Solution sans bonus
+// const helloWorld = <h1>Hello <span>{firstName[0].toUpperCase() + firstName.slice(1)}</span> <span className="red-text">{lastName.toUpperCase()}</span></h1>;
 
-ReactDOM.render(helloWorld, document.querySelector('#app'));
+// ReactDOM.render(helloWorld, document.querySelector('#app'));
+
+function FirstName(props) {
+    return <span>{props.name[0].toUpperCase() + props.name.slice(1)}</span>;
+}
+
+function LastName(props) {
+    return <span class ="red-text">{props.name.toUpperCase()}</span>;
+}
+
+const element = <h1>Hello <FirstName name="rémi"/> <LastName name= "joly"/></h1>
+
+ReactDOM.render(element, document.querySelector('#app'));
